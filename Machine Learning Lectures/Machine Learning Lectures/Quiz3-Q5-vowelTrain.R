d642955e183d79f16c04fe760a1225330f6fa7f6
alexspiers <advspiers@gmail.com>
2015-10-18 14:45:58 +0100

    added random forest

---
+++ Machine Learning Lectures/Machine Learning Lectures/Quiz3-Q5-vowelTrain.R
@@ -0,0 +1,7 @@
+library(ElemStatLearn)
+data(vowel.train)
+data(vowel.test)
+vowel.train$y <- as.factor(vowel.train$y)
+vowel.test$y <- as.factor(vowel.test$y)
+set.seed(33833)
+rf<-train(outcome ~ ., data=vowel.train, method="rf", prox=TRUE, ntree=500) 
